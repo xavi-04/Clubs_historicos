b1cf3f1f39df4a68c6425edce92a6a45772f9535
xavi-04 <00048219@uca.edu.sv>
2024-11-22 02:53:51 -0600

    Add: jest.config.js file and modify others files

--- linuxweb/src/setupTests.js
+++ linuxweb/src/setupTests.js
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+const originalError = console.error;
+console.error = (...args) => {
+  if (args[0] && args[0].includes('`ReactDOMTestUtils.act` is deprecated')) {
+    return; // Ignora el warning específico
+  }
+  originalError(...args);
+};
