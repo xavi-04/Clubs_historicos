f929ca042d067c73785fdf3abe38ff5cf8789eaf
xavi-04 <00048219@uca.edu.sv>
2024-11-28 02:40:52 -0600

    modify app

--- linuxweb/src/App.js
+++ linuxweb/src/App.js
@@ -6,10 +6,10 @@ const equipos = [
   {
     nombre: "Real Madrid",
     escudo: "/images/real_madrid.png",
-    //color: "#febd02", // Amarillo dorado (color de la corona)
-    borderTop: "8px solid transparent", // Transparent para el gradiente
+    color: "#febd02", // Amarillo dorado (color de la corona)
+    /*borderTop: "8px solid transparent", // Transparent para el gradiente
     background: "linear-gradient(90deg, #ffffff, #febd02)", // Blanco y Oro
-    background: "linear-gradient(90deg, #ffffff, #5f4b8b)", // Blanco y morado
+    background: "linear-gradient(90deg, #ffffff, #5f4b8b)", // Blanco y morado*/
     info: `
       Fundado el 6 de marzo de 1902, siendo su estadio, el Santiago Bernabéu, es un ícono del fútbol internacional.
       El Real Madrid es el club con más títulos de Champions League (15). Ha ganado 35 títulos de La Liga, 19 Copas del Rey y 5 mundiales de clubes considerandolo como el mejor club del siglo XX por la FIFA. 
@@ -20,9 +20,9 @@ const equipos = [
   {
     nombre: "AC Milan",
     escudo: "/images/milan.png",
-    // color: "8px solid #d32f2f", // Rojo (tono más suave)
-    borderTop: "8px solid transparent", // Transparent para el gradiente
-    background: "linear-gradient(90deg, #ce2324, #000000)", // Gradiente rojo y negro
+    color: "8px solid #d32f2f", // Rojo (tono más suave)
+    /*borderTop: "8px solid transparent", // Transparent para el gradiente
+    background: "linear-gradient(90deg, #ce2324, #000000)", // Gradiente rojo y negro*/
     info: `
       Fundado el 16 de diciembre de 1899, el AC Milan es uno de los clubes más exitosos de la historia con 7 Champions League, 19 títulos de la Serie A y 5 Copas de Italia. 
       Su estadio, el San Siro (también llamado Stadio Giuseppe Meazza), es compartido con el Inter de Milán.
@@ -32,9 +32,9 @@ const equipos = [
   {
     nombre: "FC Barcelona",
     escudo: "/images/barcelona.png",
-    //color: "#a50044", // Rojo granate (color del uniforme)
-    borderTop: "8px solid transparent", // Transparent para el gradiente
-    background: "linear-gradient(90deg, #a50044, #0033a0)", // Granate y Azul
+    color: "#a50044", // Rojo granate (color del uniforme)
+    /*borderTop: "8px solid transparent", // Transparent para el gradiente
+    background: "linear-gradient(90deg, #a50044, #0033a0)", // Granate y Azul*/
     info: `
       Fundado el 29 de noviembre de 1899, el Barcelona ha ganado 5 Champions League, 27 títulos de La Liga y 31 Copas del Rey.
       Es famoso por su academia 'La Masía', que ha formado a jugadores como Messi, Xavi e Iniesta. Su estadio, el Camp Nou, es uno de los más grandes e icónicos. 
@@ -44,9 +44,9 @@ const equipos = [
   {
     nombre: "Liverpool",
     escudo: "/images/liverpool.png",
-    //color: "#c8102e", // Rojo vibrante (color del uniforme)
-    borderTop: "8px solid transparent", // Transparent para el gradiente
-    background: "linear-gradient(90deg, #c8102e, #ffffff)", // Rojo y Blanco
+    color: "#c8102e", // Rojo vibrante (color del uniforme)
+    /*borderTop: "8px solid transparent", // Transparent para el gradiente
+    background: "linear-gradient(90deg, #c8102e, #ffffff)", // Rojo y Blanco*/
     info: `
       Fundado el 3 de junio de 1892, el Liverpool es el club más exitoso de Inglaterra en competiciones europeas, con 6 Champions League y ganando 19 títulos de la Premier League y 8 Copas nacionales. 
       Ha tenido jugadores icónicos como Steven Gerrard, Kenny Dalglish e Ian Rush, quienes marcaron épocas gloriosas en la historia del club.
@@ -57,9 +57,9 @@ const equipos = [
   {
     nombre: "FC Bayern",
     escudo: "/images/bayern.png",
-    //color: "8px solid #c62828", // Rojo (tono más fuerte)
-    borderTop: "8px solid transparent", // Transparent para el gradiente
-    background: "linear-gradient(90deg, #dc052d, #005bac)", // Gradiente rojo y azul
+    color: "8px solid #c62828", // Rojo (tono más fuerte)
+    /*borderTop: "8px solid transparent", // Transparent para el gradiente
+    background: "linear-gradient(90deg, #dc052d, #005bac)", // Gradiente rojo y azul*/
     info: `
       Fundado el 27 de febrero de 1900, el Bayern Múnich domina el fútbol alemán con 33 títulos de la Bundesliga y 6 Champions League.
       Logró el segundo sextete de la historia del fútbol, igualando al FC Barcelona en 2009. Su estadio, el Allianz Arena, es famoso por su arquitectura única.
@@ -69,9 +69,9 @@ const equipos = [
   {
     nombre: "Manchester United",
     escudo: "/images/Manchester_United.png",
-    //color: "8px solid #b71c1c", // Rojo (tono más oscuro)
-    borderTop: "8px solid transparent", // Transparent para el gradiente
-    background: "linear-gradient(90deg, #da291c, #febd02)", // Gradiente rojo y amarillo
+    color: "8px solid #b71c1c", // Rojo (tono más oscuro)
+   /* borderTop: "8px solid transparent", // Transparent para el gradiente
+    background: "linear-gradient(90deg, #da291c, #febd02)", // Gradiente rojo y amarillo*/
     info: `
       Fundado en 1878, el Manchester United ha ganado 20 títulos de liga, 12 FA Cups y 3 Champions League.
       Su estadio, Old Trafford, es conocido como "El Teatro de los Sueños". es uno de los más icónicos del fútbol mundial.
@@ -89,11 +89,11 @@ function App() {
       </header>
       <div className="tarjetas-container">
         {equipos.map((equipo, index) => (
-          /*<div
+          <div
             key={index}
             className="tarjeta"
             style={{ borderTop: `8px solid ${equipo.color}` }}
-          >*/
+          >
           <div
             key={index}
             className="tarjeta"
@@ -114,7 +114,7 @@ function App() {
               ))}
             </div>
           </div>
-        ))}
+        </div>))}
       </div>
     </div>
   );
