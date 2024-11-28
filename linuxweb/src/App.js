82641ba3bba534f729e88d8cbff1c90cecb80e70
xavi-04 <00048219@uca.edu.sv>
2024-11-27 21:22:01 -0600

    modify app

--- linuxweb/src/App.js
+++ linuxweb/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 
 // Datos de los equipos
@@ -29,43 +29,10 @@ const equipos = [
     color: "#a50044", // Rojo granate (color del uniforme)
     info: `
       Fundado el 29 de noviembre de 1899, el Barcelona ha ganado 5 Champions League, 27 títulos de La Liga y 31 Copas del Rey.
-      Es conocido por su academia 'La Masía', de donde han salido jugadores como Messi, Xavi e Iniesta. 
-      Su estadio, el Spotify Camp Nou, Barcelona (capacidad: 99,354), es uno de los más icónicos. Su lema es "Més que un club" ("Más que un club"), 
-      reflejando su importancia cultural y social en Cataluña. En 2009, se convirtió en el primer club en ganar un sextete (6 títulos en una temporada).
+      Es famoso por su academia 'La Masía', de donde salieron jugadores como Messi, Xavi e Iniesta. Su estadio, el Camp Nou, tiene capacidad para 99,354 espectadores.
+      Ha sido el primer club en ganar un sextete y se considera más que un club debido a su impacto social y cultural en Cataluña.
     `,
   },
-  /*
-  {
-    nombre: "Liverpool",
-    escudo: "/images/liverpool.png",
-    color: "#c8102e", // Rojo vibrante (color del uniforme)
-    info: `
-      Fundado el 3 de junio de 1892, el Liverpool es el club más exitoso de Inglaterra en competiciones europeas con 6 Champions League (Equipo ingles con más títulos europeos).
-      Ha ganado 19 títulos de la Premier League. Ha tenido jugadores icónicos como Steven Gerrard, Kenny Dalglish e Ian Rush y 
-      es famoso por su estadio Anfield, Liverpool (capacidad: 54,074) y su himno "You'll Never Walk Alone".
-    `,
-  },
-  {
-    nombre: "FC Bayern",
-    escudo: "/images/bayern.png",
-    color: "#dc052d", // Rojo Bayern
-    info: `
-      Fundado el 27 de febrero de 1900, el Bayern Múnich domina el fútbol alemán con 33 títulos de la Bundesliga y 6 Champions League. Logró el segundo sextete de la historia del fútbol, igualando al FC Barcelona en 2009.
-      Su estadio, el Allianz Arena, Múnich (capacidad: 75,024) es conocido por su arquitectura única. El club también es reconocido por su 
-      consistencia y talento. Ha sido el hogar de figuras como Franz Beckenbauer, Gerd Müller, y Philipp Lahm asi como tambien su academia juvenil es una 
-      de las más exitosas de Europa, produciendo estrellas como Thomas Müller y Bastian Schweinsteiger.
-    `,
-  },
-  {
-    nombre: "Manchester United",
-    escudo: "/images/Manchester_United.png",
-    color: "#da291c", // Rojo intenso (color del uniforme)
-    info: `
-      Fundado en 1878, el Manchester United es uno de los clubes más populares del mundo. Ha ganado 20 títulos de liga, 
-      12 FA Cups y 3 Champions League. Su estadio, Old Trafford, es conocido como "El Teatro de los Sueños".
-    `,
-  },
-  */
 ];
 
 function App() {
